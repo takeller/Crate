9149d967db66f32dd7ca043e97c2e09e4005bc9a
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-24 15:16:54 -0600

    Adds update mutation

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,7 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-import { create, remove } from './resolvers'
+import { create, update, remove } from './resolvers'
 
 // Create
 export const userSignup = {
@@ -25,6 +25,43 @@ export const userSignup = {
     }
   },
   resolve: create
+}
+
+// User update
+export const userUpdate = {
+  type: UserType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    },
+
+    name: {
+      name: 'name',
+      type: GraphQLString
+    },
+
+    email: {
+      name: 'email',
+      type: GraphQLString
+    },
+
+    description: {
+      name: 'description',
+      type: GraphQLString
+    },
+
+    address: {
+      name: 'address',
+      type: GraphQLString
+    },
+
+    image: {
+      name: 'image',
+      type: GraphQLString
+    }
+  },
+  resolve: update
 }
 
 // Remove
