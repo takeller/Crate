1e4bc5086ccd29f06206baffff9e0fe06d4d1f11
Taylor Keller <takeller17@gmail.com>
2020-08-25 15:26:58 -0600

    Adds Delivery model and updates subscription to have many deliveries

--- code/api/src/modules/subscription/model.js
+++ code/api/src/modules/subscription/model.js
@@ -14,6 +14,7 @@ module.exports = function(sequelize, DataTypes) {
   Subscription.associate = function(models) {
     Subscription.belongsTo(models.User)
     Subscription.belongsTo(models.Crate)
+    Subscription.hasMany(models.Delivery)
   }
 
   return Subscription
