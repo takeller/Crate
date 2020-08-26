63ae56ab37a3f2e69b0ca5034427762378b0958c
Taylor Keller <takeller17@gmail.com>
2020-08-26 12:52:54 -0600

    Adds delivery mutations

---
+++ code/api/src/modules/delivery/mutations.js
@@ -0,0 +1,52 @@
+// Imports
+import { GraphQLInt } from 'graphql'
+
+// App Imports
+import DeliveryType from './types'
+// import { ... } from './resolvers'
+
+// Delivery Create
+export const deliveryCreate = {
+  type: DeliveryType,
+  args: {
+    deliveryDate: {
+      name: 'deliveryDate',
+      type: GraphQLString
+    },
+
+    subscriptionId: {
+      name: 'subscriptionId',
+      type: GraphQLInt
+    }
+  },
+  resolve: create
+}
+
+// Delivery Update
+export const deliveryUpdate = {
+  type: DeliveryType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    },
+
+    deliveryDate: {
+      name: 'deliveryDate',
+      type: GraphQLString
+    }
+  },
+  resolve: update
+}
+
+// Delivery Remove
+export const deliveryRemove = {
+  type: DeliveryType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    }
+  },
+  resolve: remove 
+}
