cd13908550078349e58d73adede06ef74c49d95f
Server <server@server.com>
2020-08-26 17:31:20 -0600

    Add test file template

---
+++ code/web/src/modules/user/Form.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
+import "@testing-library/jest-dom";
+import Form from "./Form";
+
+describe('Form', () => {
+
+    it('Should render the Form component', () => {
+        // const { getByPlaceholderText, getByRole} = render(<MemoryRouter><Form /></MemoryRouter>)
+        // const titleInput = getByPlaceholderText('Title...')
+        // const urlInput = getByPlaceholderText('URL to Shorten...')
+        // const submitButton = getByRole('button')
+        // expect(titleInput).toBeInTheDocument()
+        // expect(urlInput).toBeInTheDocument()
+        // expect(submitButton).toBeInTheDocument()
+    })
+
+
+})    
---
+++ code/web/src/modules/user/Profile.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
+import "@testing-library/jest-dom";
+import Profile from "./Profile";
+
+describe('Profile', () => {
+
+    it('Should render the Profile component', () => {
+        // const { getByPlaceholderText, getByRole} = render(<MemoryRouter><Form /></MemoryRouter>)
+        // const titleInput = getByPlaceholderText('Title...')
+        // const urlInput = getByPlaceholderText('URL to Shorten...')
+        // const submitButton = getByRole('button')
+        // expect(titleInput).toBeInTheDocument()
+        // expect(urlInput).toBeInTheDocument()
+        // expect(submitButton).toBeInTheDocument()
+    })
+
+
+})   
