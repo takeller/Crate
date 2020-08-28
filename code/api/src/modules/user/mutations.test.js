3b43c2c1cacdefb3802ac3da7fc4f3311d4bb333
Taylor Keller <takeller17@gmail.com>
2020-08-28 11:50:08 -0600

    Adds teardown for destroying the user created in a test

--- code/api/src/modules/user/mutations.test.js
+++ code/api/src/modules/user/mutations.test.js
@@ -2,6 +2,7 @@ import request from 'supertest'
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema'
+import models from '../../setup/models'
 
 describe('user mutations', () => {
   let server;
@@ -16,18 +17,22 @@ describe('user mutations', () => {
         graphiql: false,
       })
     )
-  })
+  });
+
+  afterAll( async() => {
+    let user = await models.User.findOne({ where: { email: "test@email.com" } })
+    return user.destroy();
+  });
 
   it('creates a user', async () => {
     const response = await request(server)
     .post('/')
     .send({ query: ' mutation{ userSignup(name: "Test Name", email: "test@email.com", password: "test") { email name }}' })
-    // .expect(200)
-    console.log(response.body)
+    .expect(200)
 
     expect(response.body.data.userSignup.email).toEqual('test@email.com')
     expect(response.body.data.userSignup.name).toEqual('Test Name')
-  })
-})
+  });
 
-// Needs to add teardown! 
+  
+})
