8ffa41882031d70b3e92472ef1ad9a7dc270d4a9
Taylor Keller <takeller17@gmail.com>
2020-08-28 14:19:24 -0600

    Sets up auth for user mutations and add userUpdate test

--- code/api/src/modules/user/mutations.test.js
+++ code/api/src/modules/user/mutations.test.js
@@ -3,20 +3,36 @@ import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema'
 import models from '../../setup/models'
+import authentication from '../../setup/authentication'
 
 describe('user mutations', () => {
   let server;
+  let token;
 
-  beforeAll(() => {
+  beforeAll( async () => {
     server = express();
+    server.use(authentication);
 
     server.use(
       '/',
-      graphqlHTTP({
+      graphqlHTTP( request => ({
         schema: schema,
         graphiql: false,
-      })
-    )
+        context: {
+          auth: {
+            user: request.user,
+            isAuthenticated: request.user && request.user.id > 0
+          }
+        }
+      }))
+    );
+
+    let userLoginDetails = await request(server)
+    .get('/')
+    .send({ query: '{ userLogin(email: "user@crate.com", password: "123456") { user{email name} token }}'})
+    .expect(200)
+
+    token = userLoginDetails.body.data.userLogin.token;
   });
 
   afterAll( async() => {
@@ -34,5 +50,14 @@ describe('user mutations', () => {
     expect(response.body.data.userSignup.name).toEqual('Test Name')
   });
 
-  
+  it('updates a user', async () => {
+    const response = await request(server)
+    .post('/')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ query: ' mutation{ userUpdate(description: "Test Description", address: "Test Address") { name email description address }}'})
+    .expect(200)
+
+    expect(response.body.data.userUpdate.description).toEqual('Test Description')
+    expect(response.body.data.userUpdate.address).toEqual('Test Address')
+  })
 })
