4e008165211bad5035d6ccd96e3798bf2901ccea
Alex Sodemann <asodemann18@gmail.com>
2020-08-29 14:28:56 -0600

    Fix some spacing on the profile page

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -37,13 +37,13 @@ const Profile = (props) => (
       </Grid>
       <Grid>
       <GridCell style={{ padding: '2em', textAlign: 'left' }}>
-        <ImageTile width={300} height={300} image={`http://localhost:8000${props.user.details.image}`} alt='profile picture'/>
+        <ImageTile width={300} height={300} style={{ marginBottom: '0.5em' }} image={`http://localhost:8000${props.user.details.image}`} alt='profile picture'/>
         <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
+        <H4 style={{ color: grey2, marginBottom: '0.5em' }}>{props.user.details.email}</H4>
         <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.description}</H4>
-        <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.address}</H4>
-        <H4 style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</H4>
+        <H4 style={{ marginBottom: '1em' }}>{props.user.details.address}</H4>
         <H4>Update your profile using the form below!</H4>
-        <Form />
+        <Form/>
         <Link to={userRoutes.subscriptions.path}>
           <Button theme="primary">Subscriptions</Button>
         </Link>
