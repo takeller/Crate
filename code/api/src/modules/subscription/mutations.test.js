b290fd6139d57a7bb64b7b5641dfbf6d27a516e8
Taylor Keller <takeller17@gmail.com>
2020-08-28 18:06:26 -0600

    Adds subscription mutation tests

--- code/api/src/modules/subscription/mutations.test.js
+++ code/api/src/modules/subscription/mutations.test.js
@@ -31,40 +31,58 @@ describe('subscription queries', () => {
 
     let userLoginDetails = await request(server)
     .get('/')
-    .send({ query: '{ userLogin(email: "user@crate.com", password: "123456") { user{email name} token }}'})
+    .send({ query: '{ userLogin(email: "user@crate.com", password: "123456") { user{id email name} token }}'})
     .expect(200)
 
     token = userLoginDetails.body.data.userLogin.token;
     testUserId = userLoginDetails.body.data.userLogin.user.id;
     testSubscriptions = [
-    models.Subscription.create({ crateId: 1, userId: testUserId }),
-    models.Subscription.create({ crateId: 2, userId: testUserId }),
-    models.Subscription.create({ crateId: 3, userId: testUserId }),
-    models.Subscription.create({ crateId: 1, userId: 1 })
+    await models.Subscription.create({ crateId: 1, userId: testUserId }),
+    await models.Subscription.create({ crateId: 2, userId: testUserId }),
+    await models.Subscription.create({ crateId: 3, userId: testUserId }),
+    await models.Subscription.create({ crateId: 1, userId: 1 })
     ]
-  })
+  });
+
+  afterAll( async() => {
+    let latestSub = await models.Subscription.findAll({
+      limit: 1,
+      order: [ [ 'id', 'DESC' ]]
+    })
+
+    latestSub[0].destroy();
+    testSubscriptions.forEach(deleteSubscriptions)
+    async function deleteSubscriptions(subscription) {
+      let sub = await models.Subscription.findOne({ where: {id: subscription.dataValues.id}})
+      if(sub != null){
+        sub.destroy();
+      }
+    }
+  });
 
   it('creates a subscription', async () => {
     const response = await request(server)
     .post('/')
     .set('Authorization', `Bearer ${token}`)
-    .send({ query: ' mutation{ subscriptionCreate(crateId: 5) { user{ id name } crate{ id } }}'})
+    .send({ query: ' mutation{ subscriptionCreate(crateId: 5) { id user{ id name } crate{ id } }}'})
     .expect(200)
 
-    expect(response.body.data.subscriptionCreate.crate.id).toEqual(5)
-    expect(response.body.data.subscriptionCreate.user.id).toEqual(testUserId)
+    let sub = await models.Subscription.findOne({ where: { id: response.body.data.subscriptionCreate.id}})
+
+    expect(sub.dataValues.crateId).toEqual(5)
+    expect(sub.dataValues.userId).toEqual(testUserId)
   })
 
   it('removes a subscription', async () => {
     const response = await request(server)
     .post('/')
     .set('Authorization', `Bearer ${token}`)
-    .send({ query: ` mutation{ subscriptionRemove( id: ${testSubscriptions[1]}) { id } }`})
+    .send({ query: ` mutation{ subscriptionRemove( id: ${testSubscriptions[1].dataValues.id}) { id } }`})
     .expect(200)
 
     expect(response.body.data.subscriptionRemove.id).toEqual(null)
 
-    let allSubscriptions = models.Subscription.findAll()
-    expect(testSubscriptions.length - allSubscriptions.length).toEqual(1)
+    let testSubscription = await models.Subscription.findOne({ where: { id: testSubscriptions[1].dataValues.id}})
+    expect(testSubscription).toEqual(null)
   })
 })
